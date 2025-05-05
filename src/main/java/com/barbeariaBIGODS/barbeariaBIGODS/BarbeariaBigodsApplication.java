442fa2de671b47ac4d1c05f7be6078d96ec15905
alnsimao <alansimao9a1@gmail.com>
2025-05-05 19:01:07 -0300

    First Commit from PROJECT

---
+++ src/main/java/com/barbeariaBIGODS/barbeariaBIGODS/BarbeariaBigodsApplication.java
@@ -0,0 +1,13 @@
+package com.barbeariaBIGODS.barbeariaBIGODS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaBigodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaBigodsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barbeariaBIGODS/barbeariaBIGODS/BarbeariaBigodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbeariaBIGODS.barbeariaBIGODS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaBigodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
