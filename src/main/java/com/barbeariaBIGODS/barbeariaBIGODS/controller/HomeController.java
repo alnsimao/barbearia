5b610d0e1273c9d7f34cfa8384a829d9d085f83c
alnsimao <alansimao9a1@gmail.com>
2025-05-10 10:48:24 -0300

    Atualização do projeto todo, agora a página atualiza automaticamente e todas as pastas estão corretas

--- src/main/java/com/barbeariaBIGODS/barbeariaBIGODS/controller/HomeController.java
+++ src/main/java/com/barbeariaBIGODS/barbeariaBIGODS/controller/HomeController.java
@@ -1,14 +1,27 @@
-package controller;
+package com.barbeariaBIGODS.barbeariaBIGODS.controller;
+
+
 
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+
 @Controller
+
 public class HomeController {
-	
-	@GetMapping("/home")
-	public String index() {
-		return "index";
-	}
+
+
+@GetMapping("/home")
+
+public String index() {
+
+return "index";
 
 }
+
+
+
+}
+
